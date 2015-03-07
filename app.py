0feffcdadbaecaebeef58522dabd687b8c751cda
Blake Johnson <johnsonblake1@gmail.com>
2015-03-07 11:35:11 +0000

    initial models

--- app.py
+++ app.py
@@ -12,6 +12,20 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+class Item(db.Model):
+    __tablename__ = 'item'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64))
+    description = db.Column(db.Text)
+    transactions = db.relationship("Transaction", backref = "item")
+
+class Transaction(db.Model):
+    __tablename__ = 'transaction'
+    id = db.Column(db.Integer, primary_key = True)
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'))
+    time = db.Column(db.DateTime)
+    quantity = db.Column(db.Integer)
+
 if __name__ == '__main__':
     app.debug = True
     manager.run()
