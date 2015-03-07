fcd7f1eef1ee24a69f24f546df8574be4410b68c
Blake Johnson <johnsonblake1@gmail.com>
2015-03-07 17:03:06 +0000

    basic json returns for all items and transactions

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from datetime import datetime
+
+from flask import Flask, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
@@ -19,12 +21,38 @@ class Item(db.Model):
     description = db.Column(db.Text)
     transactions = db.relationship("Transaction", backref = "item")
 
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __repr__(self):
+        return '<Item %r>' % self.name
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
 class Transaction(db.Model):
     __tablename__ = 'transaction'
     id = db.Column(db.Integer, primary_key = True)
     item_id = db.Column(db.Integer, db.ForeignKey('item.id'))
     time = db.Column(db.DateTime)
     quantity = db.Column(db.Integer)
+
+    def __init__(self, item_id, quantity):
+        self.item_id = item_id
+        self.time = datetime.now()
+        self.quantity = quantity
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+@app.route('/items', methods=['GET'])
+def get_items():
+    return jsonify({'items': list(map(lambda x: x.as_dict(), Item.query.all()))})
+
+@app.route('/transactions', methods=['GET'])
+def get_transactions():
+    return jsonify({'items': list(map(lambda x: x.as_dict(), Transaction.query.all()))})
 
 if __name__ == '__main__':
     app.debug = True
