a23c4a6aad0de7975ac721d313efd0281bb48e1f
Blake Johnson <johnsonblake1@gmail.com>
2015-03-13 17:15:30 +0000

    consolidate to one model to get two way data binding, add adjustment option

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -11,26 +11,21 @@ diabetesControllers.controller('ItemListCtrl', ['$scope', '$http',
                 });
             };
         });
-    }]);
-
-diabetesControllers.controller('TransactionCtrl', ['$scope', '$http',
-    function ($scope, $http) {
-        $http.get('items').success(function (data) {
-            $scope.chooseItem = data.items;
-        });
-        $scope.add = function (transaction) {
+        $scope.add_txn = function (transaction) {
             var newTxn = {
                 item_id : $scope.transaction.id,
-                quantity : $scope.transaction.quantity
+                quantity : $scope.transaction.quantity,
+                adjustment : $scope.transaction.adjustment
             };
             var res = $http.post('/transactions', newTxn);
             res.success(function(data, status, headers, config) {
+                $scope.items[newTxn.item_id - 1].count += newTxn.quantity;
                 console.log(newTxn);
             });
             res.error(function(data, status, headers, config) {
                 alert( "failure message: " + JSON.stringify({data: data}));
             });
-            $scope.item_id = '';
-            $scope.quantity = '';
-        };
+            $scope.transaction.id = '';
+            $scope.transaction.quantity = '';
+        }
     }]);
